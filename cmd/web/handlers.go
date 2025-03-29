bbf15a1f2c3e6b7eb22ffa226e93dec3e8922bcb
KryptonBD <foysol.sahi@gmail.com>
2025-03-29 12:50:33 +0100

    feat: templates and middleware

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,4 +2,30 @@ package main
 
 import "net/http"
 
-func (app *Config) HomePage(responseWrite http.ResponseWriter, req *http.Request) {}
+func (app *Config) HomePage(responseWriter http.ResponseWriter, req *http.Request) {
+	app.render(responseWriter, req, "home.page.gohtml", nil)
+}
+
+func (app *Config) LoginPage(responseWriter http.ResponseWriter, req *http.Request) {
+	app.render(responseWriter, req, "login.page.gohtml", nil)
+}
+
+func (app *Config) PostLoginPage(responseWriter http.ResponseWriter, req *http.Request) {
+
+}
+
+func (app *Config) LogoutPage(responseWriter http.ResponseWriter, req *http.Request) {
+
+}
+
+func (app *Config) RegisterPage(responseWriter http.ResponseWriter, req *http.Request) {
+	app.render(responseWriter, req, "register.page.gohtml", nil)
+}
+
+func (app *Config) PostRegisterPage(responseWriter http.ResponseWriter, req *http.Request) {
+
+}
+
+func (app *Config) ActivateAccountPage(responseWriter http.ResponseWriter, req *http.Request) {
+
+}
---
+++ cmd/web/render.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"time"
+)
+
+var pathToTemplate = "./cmd/web/templates"
+
+type TemplateData struct {
+	StringMap     map[string]string
+	IntMap        map[string]int
+	FloatMap      map[string]float64
+	Data          map[string]any
+	Flash         string
+	Warning       string
+	Error         string
+	Authenticated bool
+	Now           time.Time
+}
+
+func (app *Config) render(responseWriter http.ResponseWriter, req *http.Request, t string, td *TemplateData) {
+	partials := []string{
+		fmt.Sprintf("%s/base.layout.gohtml", pathToTemplate),
+		fmt.Sprintf("%s/header.partial.gohtml", pathToTemplate),
+		fmt.Sprintf("%s/navbar.partial.gohtml", pathToTemplate),
+		fmt.Sprintf("%s/footer.partial.gohtml", pathToTemplate),
+		fmt.Sprintf("%s/alerts.partial.gohtml", pathToTemplate),
+	}
+
+	var templateSlice []string
+
+	templateSlice = append(templateSlice, fmt.Sprintf("%s/%s", pathToTemplate, t))
+	templateSlice = append(templateSlice, partials...)
+
+	if td == nil {
+		td = &TemplateData{}
+	}
+
+	tmpl, err := template.ParseFiles(templateSlice...)
+
+	if err != nil {
+		app.ErrorLog.Println(err)
+		http.Error(responseWriter, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.Execute(responseWriter, app.AppDefaultData(td, req)); err != nil {
+		app.ErrorLog.Println(err)
+		http.Error(responseWriter, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+}
+
+func (app *Config) AppDefaultData(td *TemplateData, req *http.Request) *TemplateData {
+	td.Flash = app.Session.PopString(req.Context(), "flash")
+	td.Warning = app.Session.PopString(req.Context(), "warning")
+	td.Error = app.Session.PopString(req.Context(), "error")
+
+	if app.IsAuthenticated(req) {
+		td.Authenticated = true
+	}
+	td.Now = time.Now()
+
+	return td
+}
+
+func (app *Config) IsAuthenticated(req *http.Request) bool {
+	return app.Session.Exists(req.Context(), "userId")
+}
